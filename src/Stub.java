336276eabe89c3fc548cf3675cfdae07ab3111ae
Nicholas Ellul <nicholas.ellul1@gmail.com>
2016-10-11 14:51:24 -0400

    Commit

---
+++ src/Stub.java
@@ -0,0 +1,13 @@
+
+public class Stub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vehicle ferarri = new Vehicle("2FST4U","Cherry Red",2,350);
+		Vehicle honda = new Vehicle("2SLO4U","Blue",4,200);
+		
+		honda.printState();
+		ferarri.printState();
+	}
+
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,56 @@
+
+public class Vehicle {
+
+	private String _licensePlateNumber;
+	private String _colour;
+	private int _numberOfDoors;
+	private double _speed = 0;
+	private double _maxSpeed;
+
+	public Vehicle(String plate, String colour, int numberOfDoor, double maxVelocity){
+		this._licensePlateNumber = plate;
+		this._colour = colour;
+	    this._numberOfDoors = numberOfDoor;
+	    this._maxSpeed = maxVelocity;
+	}
+	
+	
+	public void printState(){
+		
+		System.out.println("Speed: " + this._speed);
+		System.out.println("Max Speed: " + this._maxSpeed);
+		System.out.println("Colour: " + this._colour);
+		System.out.println("# of Doors: " + this._numberOfDoors);
+		System.out.println("License Plate Number: " + this._licensePlateNumber);
+		System.out.println();
+	}
+	
+	
+	protected void accelerate(double accelerateAmount){
+		
+		if(this._speed <= this._maxSpeed - accelerateAmount){
+			
+			this._maxSpeed += accelerateAmount;
+			
+		}
+		
+	}
+	
+	
+	protected void brake(double brakeAmount){
+		
+		if(this._speed >= brakeAmount){
+			
+			this._maxSpeed -= brakeAmount;
+			
+		}
+		else if(this._speed <= brakeAmount){
+			
+			this._speed = 0;
+			
+		}	
+		
+	}
+	
+	
+}
